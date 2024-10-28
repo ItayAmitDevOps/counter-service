f27e8d66641b62ec59cd14cc995cdfd40348351e
ItayAmitDevOps <itay.amit23@gmail.com>
2024-10-28 17:02:31 +0200

    terraform conf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+locals {
+    env = "prod"
+}
+
+variable "region" {
+    default = "eu-central-1"
+}
+
+variable "availability_zone1" {
+    default = "eu-central-1a"
+}
+
+variable "availability_zone2" {
+    default = "eu-central-1b"
+}
+
+variable "eks_name" {
+    default = "eks-counter"
+}
+
+variable "eks_version" {
+    default = "1.30"
+}
